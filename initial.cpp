973d83081b5a9b1e64493e283174dea16171d4f9
Sudharshan <srini.sudharshan1@gmail.com>
2018-02-21 15:18:50 -0800

    send w/o printing data

---
+++ initial.cpp
@@ -0,0 +1,112 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<string>
+#include<cstdlib>
+#include<string.h>
+#include<sstream>
+#include<ctime>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<map>
+#define MAX_MESSAGE_LEN 65536
+
+using namespace std;
+
+typedef map<string,struct sockaddr_in> channel_type;
+struct sockaddr_in server;
+struct sockaddr_in recv_client;
+
+class client
+{
+public:
+	string message;
+	int port;
+	int next_id;
+	int prev_id;
+
+}c[50];
+
+
+
+int main()
+{
+	int i,rc,s;
+	ssize_t bytes;
+	void *data;
+	size_t len;
+	socklen_t fromlen;
+	fromlen = sizeof(recv_client);
+	//-------------------------------------------socket creation and binding
+	s = socket(PF_INET, SOCK_DGRAM, 0);
+
+	if (s < 0)
+	{
+		perror ("socket() failed\n");
+		exit(1);
+	}
+	//cerr<<endl<<"socket created";
+
+	int port=6000;
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+	server.sin_addr.s_addr = INADDR_ANY;	
+	int err;
+	err = bind(s, (struct sockaddr*)&server, sizeof server);
+	if (err < 0)
+	{
+		perror("bind failed\n");
+	}
+	else
+	{
+		printf("bound socket\n");
+	}
+	//----------------------------------------------------
+
+	while(1) //server runs for ever
+	{
+		int rc;
+		fd_set fds;
+		FD_ZERO(&fds);
+		FD_SET(s, &fds);
+		
+		//cout << "before select" << endl;
+
+		rc = select(s+1, &fds, NULL, NULL, NULL);
+		//cout << "after select" << endl;
+		
+
+		
+		if (rc < 0)
+		{
+			printf("error in select\n");
+            getchar();
+		}
+		else
+		{
+			int socket_data = 0;
+			/*
+			if (FD_ISSET(0,&fds))
+			{
+               
+				//reading from socket
+				cerr<<"Heyyy";
+				socket_data = 1;
+
+			}
+			*/
+			if (FD_ISSET(s,&fds))
+			{
+               
+				//reading from socket
+				bytes = recvfrom(s, data, len, 0, (struct sockaddr*)&recv_client, &fromlen);
+				cout<< data << endl;
+				socket_data = 1;
+				break;
+
+			}
+			
+		}	
+	}
+}
---
+++ sender.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<string>
+#include<cstdlib>
+#include<string.h>
+#include<sstream>
+#include<ctime>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<map>
+#define port 6000
+
+using namespace std;
+
+struct sockaddr_in serv_addr;
+
+int main()
+{
+	int i,rc,s;
+	s = socket(PF_INET, SOCK_DGRAM, 0);
+	if (s < 0)
+	{
+		perror ("socket() failed\n");
+		exit(1);
+	}
+	void *data;
+	size_t len;
+	ssize_t bytes;
+	string msg="sent";
+	data = &msg;
+	len = sizeof msg;
+	serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+
+	bytes = sendto(s, data, len, 0, (struct sockaddr*)&serv_addr, sizeof serv_addr);
+
+}
+	
